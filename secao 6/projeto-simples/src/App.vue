6442b5a6623369b54a57b8378b2890a3213824eb
Miliano <jrmfilho23@gmail.com>
2019-04-09 21:24:54 -0300

    Aula 86 a 99

---
+++ secao 6/projeto-simples/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="app">{{ message}}</div>
+</template>
+<script >
+export default{
+    data(){
+      return{
+        message:"Aplicacao Vue",
+      }
+    }
+}
+</script>
+<style >
+#app{
+  background-color: #474747;
+  color: #fff;
+  font-size: 34px;
+}
+
+</style>
